ce97ee87ac327ec6e8ef6a2a5bc56ed29ed7f0b5
jabbatheteus <patorockeiro62@gmail.com>
2026-03-03 08:39:16 -0300

    Add files via upload

--- projeto hotel/backend/app.py
+++ projeto hotel/backend/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
-app = Flask(_name_)
+import os
+from flask import Flask, send_from_directory
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
+
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+
+app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/" + STATIC_DIR)
 
 @app.route("/")
 def home():
-    return "Bananaaa"
+    return "mistura"
 
 app.run()
