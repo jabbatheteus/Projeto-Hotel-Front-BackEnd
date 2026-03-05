e1c9634b8710f82830fa94b061f8b74f48c5c3c9
jabbatheteus <patorockeiro62@gmail.com>
2026-03-05 11:52:28 -0300

    Add files via upload

--- projeto hotel/backend/app.py
+++ projeto hotel/backend/app.py
@@ -12,6 +12,18 @@ app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/" + STATIC_DIR
 
 @app.route("/")
 def home():
-    return "mistura"
+    return send_from_directory(FRONTEND_DIR, "index.html")
 
-app.run()
+@app.route("/consulta")
+def consulta_page():
+    return send_from_directory(FRONTEND_DIR, "consulta.html")
+
+@app.route("/alterar")
+def alterar_page():
+    return send_from_directory(FRONTEND_DIR, "alterar.html")
+
+if __name__=="__main__":
+    print("BASE_DIR:", BASE_DIR)
+    print("FRONTEND_DIR:", FRONTEND_DIR)
+    print("STATIC_DIR", STATIC_DIR)
+    app.run(debug=True)
