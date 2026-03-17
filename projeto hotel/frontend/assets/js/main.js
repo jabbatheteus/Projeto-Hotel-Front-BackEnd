6aafcb0d4835a7dea231625b9e980f62da5fd486
jabbatheteus <patorockeiro62@gmail.com>
2026-03-17 09:27:46 -0300

    Add files via upload

---
+++ projeto hotel/frontend/assets/js/main.js
@@ -0,0 +1,17 @@
+DocumentFragment.addEventListener("DOMContentLoaded", function () {
+    const formCadastro = document.getElementById("formCadastro");
+
+    formCadastro.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const dados = Object.fromEntries(
+            new FormData(formCadastro)
+        )
+
+        console.log("Dados capturados:");
+        console.log("Nome:", dados.nome);
+        console.log("Email:", dados.email);
+        console.log("Telefone", dados.telefone);
+        console.log(dados);
+    })
+})
